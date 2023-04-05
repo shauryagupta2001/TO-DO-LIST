88dd2086c70ce1df84f613288c2f9ae4c5cb3031
shauryagupta2001 <127601415+shauryagupta2001@users.noreply.github.com>
2023-04-06 00:02:45 +0530

    Update js.ts

--- js.ts
+++ js.ts
@@ -1,33 +1,14 @@
-import { Component , OnInit}from '@angular/core';
-import {MyService} from '../service/myService';
-import {of,map,filter} from 'rxjs';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-addtocart',
-  templateUrl: './addtocart.component.html',
-  styleUrls: ['./addtocart.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AddtocartComponent implements OnInit {
-  constructor(private ms:MyService){}
-  cart:number=0;
-  price="10000";
-   arr : number[] = [];
-  list = of(1,3,4,5,6,8,12);
+export class AppComponent {
   todoList: string[] = [];
   newTodoText: string = '';
-  
-  ngOnInit(){
-    this.ms.getAddToCartData().subscribe(data => {
-      this.cart = parseInt(data);
-    })
-    this.list.pipe(map(x => x*5)).subscribe(data =>{
-      this.arr.push(data);
-    })
-    // this.list.pipe(filter(x => x%2==0)).subscribe(data =>{
-    //   console.log(data);
-    // })
 
-  }
   addTodoItem() {
     if (this.newTodoText.trim()) {
       this.todoList.push(this.newTodoText);
@@ -38,6 +19,4 @@ export class AddtocartComponent implements OnInit {
   removeTodoItem(index: number) {
     this.todoList.splice(index, 1);
   }
-
-  
 }
